73c034e1dbe43ae9d6de7bb372304441ef0bf508
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-07 21:35:14 +0530

    Switch case

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -6,14 +6,26 @@ public class EmpWageBuilder
 		final int IS_FULL_TIME=1;
 		final int IS_PART_TIME=2;
 		final int EMP_RATE_PER_HR=20;
+		final int MAX_WORKINGS_DAYS=20;
 		int empHrs=0;
 		int dailyWage=0;
-		double empCheck=Math.floor(Math.random()*10)%3;
-		if (empCheck == IS_FULL_TIME)
-			empHrs=8;
-		else if (empCheck == IS_PART_TIME)
-			empHrs=4;
-		dailyWage=empHrs*EMP_RATE_PER_HR;
-		System.out.println("Daily wage of employee is:"+dailyWage);
+		for (int day=1; day<=MAX_WORKINGS_DAYS; day++)
+		{
+			double empCheck=Math.floor(Math.random()*10)%3;
+			switch((int)empCheck)
+      	{
+         	case IS_FULL_TIME:
+                  empHrs=8;
+                  break;
+         	case IS_PART_TIME:
+                  empHrs=4;
+                  break;
+         	default:
+                  empHrs=0;
+                  break;
+      	}
+		dailyWage+=empHrs;
+		}
+		System.out.println("Wage of Emp after "+MAX_WORKINGS_DAYS+" days is: "+dailyWage*EMP_RATE_PER_HR);
 	}
 }
