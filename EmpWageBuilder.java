05b650be2f1133b51d39403a85d37dc7d89eaf44
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-07 21:28:49 +0530

    Attendance check

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,7 +1,13 @@
 public class EmpWageBuilder
 {
-	public static void main(String[] args)
+	public static void main(String args[])
 	{
-		System.out.println("Welcome to master branch of emp wage computation!");
+		//System.out.println("Welcome to Employee Wage Computation Program");
+		final int IS_PRESENT=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_PRESENT)
+			System.out.println("Emp is present");
+		else
+			System.out.println("Emp is absent");
 	}
 }
